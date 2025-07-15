7f7d640fd2b90c382a57696ec49e7bd8699280d1
Israelo149 <adebiyiolanrewaju82@gmail.com>
2025-07-15 14:54:01 +0100

    feat: add main landmark, header/footer, accessibility fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Auth from './pages/Auth';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Goals from './pages/Goals';
-import Privacy from './pages/Privacy';
-import Terms from './pages/Terms';
-import Admin from './pages/Admin';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Auth from './pages/Auth'
+import Dashboard from './pages/Dashboard'
+import Goals from './pages/Goals'
+import Privacy from './pages/Privacy'
+import Terms from './pages/Terms'
+import Admin from './pages/Admin'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/goals" element={<Goals />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </main>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
