c1f21f6c4f96e1538e169f7a94caef8841d1c363
Israelo149 <adebiyiolanrewaju82@gmail.com>
2025-07-20 22:30:24 +0100

    fix: corrected main.jsx syntax and BrowserRouter setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'; // 👈 This loads your styles!
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
